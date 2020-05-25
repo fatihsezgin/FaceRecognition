6bb0a31b497bcc05e957fa2493c241c616cd769b
fatihsezgin <fatihsezgin98@hotmail.com>
2020-05-25 21:20:26 +0300

    localbinarypattern algorithm

--- camera.py
+++ camera.py
@@ -6,7 +6,12 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QDialog, QApplication
-from PyQt5.uic import loadUi 
+from PyQt5.uic import loadUi
+import facedeneme
+
+
+def get_gray_scale(frame):
+    return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
 class camera(QDialog):
@@ -17,33 +22,39 @@ class camera(QDialog):
         self.value = 1
         self.buttonOpenCamera.clicked.connect(self.onclicked)
         self.buttonCapture.clicked.connect(self.captureClicked)
+        self.buttonDetect.clicked.connect(self.detectRecognition)
 
-    #@pyqtSlot
+    # @pyqtSlot
     def onclicked(self):
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(-1)
         faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-        while(cap.isOpened()):
+        while cap.isOpened():
             ret, frame = cap.read()
-            if ret == True:
-                gray = self.get_gray_scale(frame)
+            if ret:
+                gray = get_gray_scale(frame)
                 faces = faceCascade.detectMultiScale(
-                    gray, 
-                    scaleFactor =1.1,
-                    minNeighbors =5,
-                    minSize =(30,30),
-                    flags =cv2.CASCADE_SCALE_IMAGE
+                    gray,
+                    scaleFactor=1.1,
+                    minNeighbors=5,
+                    minSize=(30, 30),
+                    flags=cv2.CASCADE_SCALE_IMAGE
                 )
-                bound = (0,0,0,0)
-                for (x, y, w, h) in faces:    
-                    self.displayImage(cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2), 1)
-                    bound=(x, y, w, h)
-                
-                if(self.capture):
+
+                bound = (0, 0, 0, 0)
+                for (x, y, w, h) in faces:
+                    self.displayImage(cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2))
+                    bound = (x, y, w, h)
+
+                self.displayImage(frame)
+
+                facedeneme.detectRecognition()
+
+                if self.capture:
                     print("capture is clicked")
                     self.value += 1
-                    grayScale = self.get_gray_scale(frame)
-                    cv2.imwrite("./%s.png" % (self.value), grayScale[y:bound[1]+bound[3], x:bound[0]+bound[2]])
+                    grayScale = get_gray_scale(frame)
+                    print(type(grayScale))
+                    cv2.imwrite("./images/%s.png" % self.value, grayScale[y:bound[1] + bound[3], x:bound[0] + bound[2]])
                     self.capture = False
                     print("image saved")
             else:
@@ -51,19 +62,14 @@ class camera(QDialog):
         cap.release()
         cv2.destroyAllWindows()
 
-
-    def get_gray_scale(self,frame):
-        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-
     def captureClicked(self):
         self.capture = True
 
-    def displayImage(self, img, window=1):
-        
+    def displayImage(self, img):
+
         qformat = QImage.Format_Indexed8
         if len(img.shape) == 3:
-            if(img.shape[2]) == 4:
+            if (img.shape[2]) == 4:
                 qformat = QImage.Format_RGBA888
             else:
                 qformat = QImage.Format_RGB888
@@ -73,7 +79,6 @@ class camera(QDialog):
         self.imgLabel.setAlignment(
             QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
         QApplication.processEvents()
-
 
 
 app = QtWidgets.QApplication(sys.argv)
---
+++ facerecognition.py
@@ -0,0 +1,139 @@
+from math import sqrt
+
+import numpy
+import cv2
+
+imgPath = '3.png'
+
+
+def get_gray_scale(img_path):
+    rgb_image = cv2.imread(img_path)
+    return cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
+
+
+def detect_faces(img_path):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray_scale = get_gray_scale(img_path)
+    return face_cascade.detectMultiScale(gray_scale, 1.2, 4)
+
+
+def crop_face(img_path, x_axis, y_axis, width, height):
+    grayScale = get_gray_scale(img_path)
+    cv2.rectangle(grayScale, (x_axis, y_axis), (x_axis + width, y_axis + height), (255, 0, 0), 1)
+    return grayScale[y_axis:y_axis + height, x_axis:x_axis + width]
+
+
+def localBinaryPattern(imagePath):
+    faces = detect_faces(imgPath)
+    maxArea = 0
+    bound = (0, 0, 0, 0)
+
+    for (x, y, w, h) in faces:
+        area = w * h
+        if area > maxArea:
+            bound = (x, y, w, h)
+
+    crop = crop_face(imgPath, bound[0], bound[1], bound[2], bound[3])
+    cropH = crop.shape[0]
+    cropW = crop.shape[1]
+    lbpImg = numpy.zeros(shape=(cropH, cropW))
+
+    for i in range(crop.shape[0]):  # traverses through height of the image
+        for j in range(crop.shape[1]):  # traverses through width of the image
+            if i != 0 and i != cropH - 1 and j != 0 and j != cropW - 1:
+                threshold = crop[i][j]
+                binary: str = ''
+                for k in range(3):
+                    for m in range(3):
+                        if k != 1 or 1 != m:
+                            if crop[i + k - 1][j + m - 1] >= threshold:
+                                binary = binary + '1'
+                            else:
+                                binary = binary + '0'
+                lbpImg[i][j] = int(binary, 2) / 255
+                #print(lbpImg[i][j] * 255)
+            else:
+                lbpImg[i][j] = crop[i][j] / 255
+    return lbpImg
+
+
+def calculate(image, refImage):
+    lbpImage1 = localBinaryPattern(image)
+    refImage = localBinaryPattern(refImage)
+
+    his1 = cal_histogram(lbpImage1)
+    his2 = cal_histogram(refImage)
+
+    print(his1)
+    print(his2)
+
+    print(compare_histograms(his1, his2))
+
+
+'''
+faces = detect_faces(imgPath)
+maxArea = 0
+bound = (0, 0, 0, 0)
+
+for (x, y, w, h) in faces:
+    area = w * h
+    if area > maxArea:
+        bound = (x, y, w, h)
+
+crop = crop_face(imgPath, bound[0], bound[1], bound[2], bound[3])
+cropH = crop.shape[0]
+cropW = crop.shape[1]
+lbpImg = numpy.zeros(shape=(cropH, cropW))
+
+for i in range(crop.shape[0]):  # traverses through height of the image
+    for j in range(crop.shape[1]):  # traverses through width of the image
+        if i != 0 and i != cropH - 1 and j != 0 and j != cropW - 1:
+            threshold = crop[i][j]
+            binary: str = ''
+            for k in range(3):
+                for m in range(3):
+                    if k != 1 or 1 != m:
+                        if crop[i + k - 1][j + m - 1] >= threshold:
+                            binary = binary + '1'
+                        else:
+                            binary = binary + '0'
+            lbpImg[i][j] = int(binary, 2) / 255
+            print(lbpImg[i][j] * 255)
+        else:
+            lbpImg[i][j] = crop[i][j] / 255
+
+'''
+
+
+def cal_histogram(lbp_img):
+    imgArea = lbp_img.shape[0] * lbp_img.shape[1]
+    histogram = numpy.zeros(shape=256)
+    for a in range(lbp_img.shape[0]):
+        for b in range(lbp_img.shape[1]):
+            index = int(lbp_img[a][b] * 255)
+            histogram[index] = histogram[index] + 1
+    for d in range(256):
+        histogram[d] = histogram[d] / imgArea
+    return histogram
+
+
+def compare_histograms(his1, his2):
+    if len(his1) != len(his2):
+        return -1
+    distance = 0
+    for ite in range(len(his2)):
+        distance = distance + sqrt((his1[ite] - his2[ite]) * (his1[ite] - his2[ite]))
+    return distance
+
+
+calculate('3.png', './histograms/deneme1.png')
+
+# hist = cal_histogram(lbpImg)
+#
+# for c in range(len(hist)):
+#     print('hist' + str(c) + ':' + str(hist[c]))
+#
+# cv2.imshow('img', lbpImg)
+# cv2.imwrite("./histograms/deneme1.png", 255 * lbpImg)
+#
+# cv2.waitKey(0)
